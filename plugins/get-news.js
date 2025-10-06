4be72b6fe70e88dfa0255492440b60d0e7270359
whatsapp-bot254 <sirbravin41@gmail.com>
2025-10-06 16:01:50 +0300

    Update get-news.js

--- plugins/get-news.js
+++ plugins/get-news.js
@@ -24,7 +24,7 @@ async (conn, mek, m, { from, reply }) => {
 ⚠️ _${article.description}_
 🔗 _${article.url}_
 
-  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ Jᴀᴡᴀᴅ TᴇᴄʜX
+  ©ᴘᴏᴡᴇʀᴇᴅ ʙʏ WHATSAPP BOT 
             `;
 
             console.log('Article URL:', article.urlToImage); // Log image URL for debugging
